4846def49b547cb6d8693f1f6fd7af914b688f16
Andy Speakman <andy.speakman@gmail.com>
2015-02-08 13:13:21 +0000

    Reversion of test
    Removed the test change made on the server side.

--- database/redirects.sql
+++ database/redirects.sql
@@ -5,4 +5,3 @@ SELECT CONCAT('RewriteRule ^/' , s.url_path , '$ /school/index/name/' , s.url_pa
   FROM school s
   JOIN school_year y ON s.school_id = y.school AND y.year = 2015
  ORDER BY s.url_path
--- Quick change to test Github...
