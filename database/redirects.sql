a6d660a2b3498d5246a3b3d414e8734567a17f1e
andyspeakman <andy.speakman@gmail.com>
2015-02-08 13:09:58 +0000

    Update redirects.sql
    Test commit.

--- database/redirects.sql
+++ database/redirects.sql
@@ -5,3 +5,4 @@ SELECT CONCAT('RewriteRule ^/' , s.url_path , '$ /school/index/name/' , s.url_pa
   FROM school s
   JOIN school_year y ON s.school_id = y.school AND y.year = 2015
  ORDER BY s.url_path
+-- Quick change to test Github...
