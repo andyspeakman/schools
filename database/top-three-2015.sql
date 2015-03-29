2350ec57533411759602718f47b163ee105c48e7
Andy Speakman <andy.speakman@gmail.com>
2015-03-29 14:18:52 +0100

    Open up voting and ranking for 2015.

---
+++ database/top-three-2015.sql
@@ -0,0 +1,191 @@
+-- Hope School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8204, 8205, 8208)
+   AND school_year = 75;
+
+-- Leamington Primary:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8211, 8218, 8232)
+   AND school_year = 76;
+
+-- Liverpool College:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8238, 8239, 8245)
+   AND school_year = 77;
+
+-- Maghull High School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8247, 8263, 8269)
+   AND school_year = 78;
+
+-- Maricourt High School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8272, 8273, 8282)
+   AND school_year = 79;
+
+-- Merton Bank Primary School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8301, 8303, 8305)
+   AND school_year = 80;
+
+-- Much Woolton Primary:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8307, 8309, 8310)
+   AND school_year = 81;
+
+-- Neston High School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8312, 8319, 8324)
+   AND school_year = 82;
+
+-- Norman Pannell School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8336, 8339, 8340)
+   AND school_year = 83;
+
+-- Ormiston Bolingbroke Academy:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8349, 8353, 8357)
+   AND school_year = 110;
+
+-- Parish C of E Primary School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8366, 8369, 8384)
+   AND school_year = 84;
+
+-- Rainhill High School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8387, 8388, 8390)
+   AND school_year = 111;
+
+-- Springwood Heath Primary:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8395, 8402, 8419)
+   AND school_year = 85;
+
+-- St Augustine of Canterbury Catholic High:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8424, 8433, 8434)
+   AND school_year = 112;
+
+-- St Chad's High School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8442, 8447, 8451)
+   AND school_year = 113;
+
+-- St Cuthberts Primary School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8460, 8463, 8465)
+   AND school_year = 86;
+
+-- St Gregorys High School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8479, 8480, 8492)
+   AND school_year = 87;
+
+-- St Hilda's CoE High School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8495, 8496, 8497)
+   AND school_year = 114;
+
+-- St John Bosco Arts College:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8500, 8504, 8506)
+   AND school_year = 115;
+
+-- St Julies Catholic High School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8524, 8528, 8531)
+   AND school_year = 116;
+
+-- St Margarets Academy:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8538, 8545, 8551)
+   AND school_year = 117;
+
+-- St Mary’s College (Crosby):
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8558, 8562, 8565)
+   AND school_year = 88;
+
+-- St Marys Catholic College (Wallasey):
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8629, 8634, 8640)
+   AND school_year = 119;
+
+-- St Michael & All Angels Catholic Primary:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8573, 8576, 8593)
+   AND school_year = 118;
+
+-- St Michael's Catholic Primary School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8597, 8598, 8607)
+   AND school_year = 89;
+
+-- St Patricks Primary School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8618, 8620, 8624)
+   AND school_year = 90;
+
+-- The Blue Coat School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8663, 8666, 8675)
+   AND school_year = 120;
+
+-- The Cavendish Academy:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8678, 8683, 8686)
+   AND school_year = 102;
+
+-- The Heath School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8687, 8688, 8691)
+   AND school_year = 121;
+
+-- The Oldershaw School Academy:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8693, 8712, 8716)
+   AND school_year = 122;
+
+-- Weatherhead High School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8723, 8730, 8734)
+   AND school_year = 91;
+
+-- West Kirby Residential School:
+UPDATE entry
+   SET rank = 1
+ WHERE entry_id IN (8736, 8737, 8742)
+   AND school_year = 124;
