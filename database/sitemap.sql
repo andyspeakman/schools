f30ffecc6def798606493835136c39e536428b6e
Andy Speakman <andy.speakman@gmail.com>
2015-04-01 21:11:05 +0100

    Fixed error in school sitemap file.

--- database/sitemap.sql
+++ database/sitemap.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT('   <url>\r\n      <loc>http://schools.dot-art.com/index/name/', s.url_path, '</loc>\r\n      <lastmod>2015-03-29</lastmod>\r\n      <priority>1.0</priority>\r\n      <changefreq>weekly</changefreq>\r\n   </url>')
+SELECT CONCAT('   <url>\r\n      <loc>http://schools.dot-art.com/school/index/name/', s.url_path, '</loc>\r\n      <lastmod>2015-03-29</lastmod>\r\n      <priority>1.0</priority>\r\n      <changefreq>weekly</changefreq>\r\n   </url>')
   FROM school s
   JOIN school_year y ON s.school_id = y.school
  WHERE y.year = 2015;
