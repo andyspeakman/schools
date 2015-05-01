6d7b8f0732240e4f71e01578e6b8d5633975d850
Andy Speakman <andy.speakman@gmail.com>
2015-05-01 19:27:00 +0100

    Turned off voting function.

--- application/controllers/SchoolController.php
+++ application/controllers/SchoolController.php
@@ -41,6 +41,7 @@ class SchoolController extends Zend_Controller_Action
     
     public function voteAction()
     {
+        /*
         $bootstrap = $this->getInvokeArg('bootstrap');
         $log = $bootstrap->getResource('log');
 
@@ -90,6 +91,7 @@ class SchoolController extends Zend_Controller_Action
         
         $this->view->returnSchoolUrl = $schoolUrl;
         $this->_helper->viewRenderer->setRender('thankyou');
+        */
     }
 
     private function _getCleanEmail($email)
