dec0a9075c2b38a9c0b6b5b8bcca1c128e2014a6
Andy Speakman <andy.speakman@gmail.com>
2017-03-11 14:57:22 +0000

    Sitemap updates

--- database/sitemap.sql
+++ database/sitemap.sql
@@ -1,4 +1,11 @@
-SELECT CONCAT('   <url><loc>http://schools.dot-art.com/school/index/name/', s.url_path, '</loc><lastmod>2016-03-12</lastmod><priority>1.0</priority><changefreq>weekly</changefreq></url>')
+SELECT CONCAT('   <url><loc>http://cheshire.schools.dot-art.com/school/index/name/', s.url_path, '</loc><lastmod>2017-03-11</lastmod><priority>1.0</priority><changefreq>weekly</changefreq></url>')
   FROM school s
   JOIN school_year y ON s.school_id = y.school
- WHERE y.year = 2016;
+ WHERE s.region = 2
+   AND y.year = 2017;
+
+SELECT CONCAT('   <url><loc>http://liverpool.schools.dot-art.com/school/index/name/', s.url_path, '</loc><lastmod>2017-03-11</lastmod><priority>1.0</priority><changefreq>weekly</changefreq></url>')
+  FROM school s
+  JOIN school_year y ON s.school_id = y.school
+ WHERE s.region = 1
+   AND y.year = 2017;
