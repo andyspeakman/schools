5aab350578541a9d99587d211ea3fb7e4845faf1
Andy Speakman <andy.speakman@gmail.com>
2019-03-28 20:57:33 +0000

    Made voting action dependent on correct status.

--- liverpool/application/controllers/SchoolController.php
+++ liverpool/application/controllers/SchoolController.php
@@ -2,7 +2,16 @@
 
 class SchoolController extends Zend_Controller_Action
 {
-    
+
+    private $_competitionStatus = false;
+
+    public function init()
+    {
+        $bootstrap = $this->getInvokeArg('bootstrap');
+        $conf = $bootstrap->getResource('config');
+        $this->_competitionStatus = $conf->competition->status;
+    }
+
     public function indexAction()
     {
         $bootstrap = $this->getInvokeArg('bootstrap');
@@ -34,6 +43,7 @@ class SchoolController extends Zend_Controller_Action
         $conf = $bootstrap->getResource('config');
         $currentYear = $conf->competition->currentYear;
         $this->view->currentYear = $currentYear;
+        $this->view->competitionStatus = $this->_competitionStatus;
 
         // Retrieve school's entries from the cache:
         $entriesCacheKey = 'school_entries_' . $schoolCacheName;
@@ -47,57 +57,57 @@ class SchoolController extends Zend_Controller_Action
     
     public function voteAction()
     {
-        /*
-        $bootstrap = $this->getInvokeArg('bootstrap');
-        $log = $bootstrap->getResource('log');
+        if ($this->_competitionStatus == 'voting') {
+            $bootstrap = $this->getInvokeArg('bootstrap');
+            $log = $bootstrap->getResource('log');
 
-        $email = $this->getRequest()->getPost('email-address');
-        if ($email == null) {
-            $log->err('SchoolController: Attempt to vote without an email-address value');
-        }
-        $cleanEmail = self::_getCleanEmail($email);
-        $log->debug('EMAIL: ' . $cleanEmail);
+            $email = $this->getRequest()->getPost('email-address');
+            if ($email == null) {
+                $log->err('SchoolController: Attempt to vote without an email-address value');
+            }
+            $cleanEmail = self::_getCleanEmail($email);
+            $log->debug('EMAIL: ' . $cleanEmail);
 
-        $schoolId = $this->getRequest()->getPost('school-id');
-        if ($schoolId == null) {
-            $log->err('SchoolController: Attempt to vote without an school-id value');
-        }
-        $log->debug('SCHOOL ID: ' . $schoolId);
-        
-        $schoolUrl = $this->getRequest()->getPost('school-url');
-        if ($schoolUrl == null) {
-            $log->err('SchoolController: Attempt to vote without a school-url value');
-        }
-        $log->debug('SCHOOL URL: ' . $schoolUrl);
+            $schoolId = $this->getRequest()->getPost('school-id');
+            if ($schoolId == null) {
+                $log->err('SchoolController: Attempt to vote without an school-id value');
+            }
+            $log->debug('SCHOOL ID: ' . $schoolId);
+            
+            $schoolUrl = $this->getRequest()->getPost('school-url');
+            if ($schoolUrl == null) {
+                $log->err('SchoolController: Attempt to vote without a school-url value');
+            }
+            $log->debug('SCHOOL URL: ' . $schoolUrl);
 
-        $entryId = $this->getRequest()->getPost('art-id');
-        if ($entryId == null) {
-            $log->err('SchoolController: Attempt to vote without an entry-id value');
-        }
-        $log->debug('ART ID: ' . $entryId);
+            $entryId = $this->getRequest()->getPost('art-id');
+            if ($entryId == null) {
+                $log->err('SchoolController: Attempt to vote without an entry-id value');
+            }
+            $log->debug('ART ID: ' . $entryId);
 
-        $artist = $this->getRequest()->getPost('artist');
-        if ($artist == null) {
-            $log->err('SchoolController: Attempt to vote without an artist value');
-        }
-        $log->debug('ARTIST: ' . $artist);
+            $artist = $this->getRequest()->getPost('artist');
+            if ($artist == null) {
+                $log->err('SchoolController: Attempt to vote without an artist value');
+            }
+            $log->debug('ARTIST: ' . $artist);
 
-        $entryTitle = $this->getRequest()->getPost('entry-title');
-        if ($entryTitle == null) {
-            $log->err('SchoolController: Attempt to vote without an entry-title value');
+            $entryTitle = $this->getRequest()->getPost('entry-title');
+            if ($entryTitle == null) {
+                $log->err('SchoolController: Attempt to vote without an entry-title value');
+            }
+            $log->debug('ENTRY TITLE: ' . $entryTitle);
+            
+            $hash = self::_getHash($cleanEmail, $entryId);
+            $manager = new Lightman_Managers_Vote();
+            $manager->add($cleanEmail, $entryId, $hash);
+            
+            $emailManager = new Lightman_Managers_Email();
+            $emailManager->sendConfirmationEmail($cleanEmail, $hash, $artist, $entryTitle);
+            
+            $this->view->returnSchoolUrl = $schoolUrl;
+            $this->_helper->viewRenderer->setRender('thankyou');
         }
-        $log->debug('ENTRY TITLE: ' . $entryTitle);
-        
-        $hash = self::_getHash($cleanEmail, $entryId);
-        $manager = new Lightman_Managers_Vote();
-        $manager->add($cleanEmail, $entryId, $hash);
-        
-        $emailManager = new Lightman_Managers_Email();
-        $emailManager->sendConfirmationEmail($cleanEmail, $hash, $artist, $entryTitle);
-        
-        $this->view->returnSchoolUrl = $schoolUrl;
-        $this->_helper->viewRenderer->setRender('thankyou');
-        */
     }
 
     private function _getCleanEmail($email)
