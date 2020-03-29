2437a65459fc9ce11befa5cc269f7965dd529b1e
Andy Speakman <andy.speakman@gmail.com>
2020-03-29 18:26:47 +0100

    Added voting summary page.

--- library/Lightman/Managers/Vote.php
+++ library/Lightman/Managers/Vote.php
@@ -4,9 +4,20 @@ require_once APPLICATION_PATH . '/models/DbTable/Vote.php';
 
 class Lightman_Managers_Vote
 {
+    private $_currentYear;
+
     const STATUS_INITIALISED = 1;
     const STATUS_CONFIRMED = 2;
     const STATUS_DELETED = 3;
+
+    public function __construct()
+    {
+        $front = Zend_Controller_Front::getInstance();
+        $bootstrap = $front->getParam('bootstrap');
+        $conf = $bootstrap->getResource('config');
+        $this->_currentYear = $conf->competition->currentYear;
+    }
+
     
     public function add($email, $entryId, $hash)
     {
@@ -53,6 +64,49 @@ class Lightman_Managers_Vote
         );
         $where = $table->getAdapter()->quoteInto('vote_id = ?', $id);
         $table->update($data, $where);
+    }
+
+    public function fetchVoteSummary()
+    {
+        $db = Zend_Registry::get('dbAdapter');
+        $sql = 'SELECT s.name AS school, e.artist AS artist, e.title AS title, COUNT(v.vote_id) AS votes
+            FROM entry e
+            JOIN school_year y ON e.school_year = y.school_year_id
+            JOIN school s ON y.school = s.school_id
+            LEFT JOIN vote v ON e.entry_id = v.entry AND v.status = ?
+            WHERE y.year = ?
+            AND e.rank IS NOT NULL
+            GROUP BY s.name, e.artist, e.title';
+        $results = $db->fetchAll($sql, array(self::STATUS_CONFIRMED, $this->_currentYear));
+        return $results;
+    }
+
+    public function fetchVoteCount()
+    {
+        $db = Zend_Registry::get('dbAdapter');
+        $sql = 'SELECT v.status AS status, COUNT(1) AS total
+            FROM vote v
+            JOIN entry e ON v.entry = e.entry_id
+            JOIN school_year y ON e.school_year = y.school_year_id
+            WHERE y.year = ?
+            GROUP BY v.status
+            ORDER BY v.status';
+        $results = $db->fetchAll($sql, array($this->_currentYear));
+        return $results;
+    }
+
+    public function fetchUnconfirmedVoters()
+    {
+        $db = Zend_Registry::get('dbAdapter');
+        $sql = 'SELECT email, date_time
+            FROM vote v
+            JOIN entry e ON v.entry = e.entry_id
+            JOIN school_year y on e.school_year = y.school_year_id
+            WHERE y.year = ?
+            AND v.status = ?
+            ORDER BY date_time';
+        $results = $db->fetchAll($sql, array($this->_currentYear, self::STATUS_INITIALISED));
+        return $results;
     }
 
 }
--- liverpool/application/controllers/VoteController.php
+++ liverpool/application/controllers/VoteController.php
@@ -42,5 +42,30 @@ class VoteController extends Zend_Controller_Action
         $manager->confirmVote($voteId);
     }
 
+    public function summaryAction()
+    {
+        $bootstrap = $this->getInvokeArg('bootstrap');
+        $log = $bootstrap->getResource('log');
+        $conf = $bootstrap->getResource('config');
+
+        if(!isset($_COOKIE['summaryAccess'])
+            || $_COOKIE['summaryAccess'] != $conf->competition->summaryAccessKey)
+        {
+            $log->alert('Unauthorised access request to Voting Summary page.');
+            $redirector = $this->_helper->getHelper('Redirector');
+            $redirector->gotoSimple('index', 'schools');
+        }
+
+        $manager = new Lightman_Managers_Vote();
+        $summaryData = $manager->fetchVoteSummary();
+        $this->view->summary = $summaryData;
+
+        $voteCount = $manager->fetchVoteCount();
+        $this->view->voteCount = $voteCount;
+
+        $unconfirmedVoters = $manager->fetchUnconfirmedVoters();
+        $this->view->unconfirmedVoters = $unconfirmedVoters;
+    }
+
 }
 
